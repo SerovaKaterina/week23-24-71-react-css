bb3b758827f2471616790eb39edc6e7b2f5e731a
SerovaKaterina <k.serova.k@yandex.ru>
2024-03-03 20:57:48 +0200

    add week24

--- tariff/src/App.js
+++ tariff/src/App.js
@@ -1,20 +1,29 @@
+import React, {useState} from 'react';
 import './App.css';
 import TariffCard from './components/TariffCard/TariffCard';
 import DataCard from './data.json';
 
 
 function App() {
+  const [selectedTariff, setSelectedTariff] = useState(null);
+
+  const handleTariffSelect = (tariffName) => {
+    setSelectedTariff(tariffName === selectedTariff ? null : tariffName);
+  };
+
   return (
     <div className="App">
      {DataCard.map((tariff) => (
         <TariffCard
+          key = {tariff.name}
           name={tariff.name}
           rate={tariff.rate}
           speed={tariff.speed}
           content={tariff.content}
-          other={tariff.other}
           color={tariff.color}
           nextcolor={tariff.nextcolor}
+          isSelected = {tariff.name === selectedTariff}
+          onSelect = {handleTariffSelect}
         />
       ))}
     </div>
--- tariff/src/components/TariffCard/TariffCard.jsx
+++ tariff/src/components/TariffCard/TariffCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from './tariffCard.module.css';
 
-function TariffCard(props) {
-    const {name, rate, speed, content, other, color, nextcolor} = props;
-    
+function TariffCard({name, rate, speed, content, color, nextcolor, isSelected, onSelect}) {
+    const handleClick = () => {
+        onSelect(name);
+    };
     return (
-        <div className= {other?styles.otherCard:styles.tariff}>
+        <div className={`${styles.tariff} ${isSelected ? styles.selected :""}`}
+        onClick = {handleClick}>
             <div className={styles.tariff_name} style={{backgroundColor:color}}><h1>{name}</h1></div>
             <div className={styles.tariff_rate} style={{backgroundColor:nextcolor}}>{rate} руб в месяц</div>
             <div className={styles.tariff_speed}>до {speed} Мбит/сек</div>
